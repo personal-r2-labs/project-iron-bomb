6a32c9554150e8017780cd8b47e432cd3913879a
Romulo Franca <romulo2franca@gmail.com>
2018-10-31 23:50:25 -0300

    Update Bomb, Player and Questions scripts

--- src/content/player.js
+++ src/content/player.js
@@ -3,15 +3,18 @@ function Player(name) {
   this.error = 0;
   this.score = 0;
   this.questions = [];
+  this.bomb = new ClockBomb();
 }
 
 Player.prototype.addQuestions = function (arr, qt) {
   if (qt >= arr.length) this.questions = arr;
   let ramdomIndex;
+  let currentIndex;
+  let obj;
   for (let i = 0; i < qt; i += 1) {
-    const currentIndex = arr.length;
+    currentIndex = arr.length;
     ramdomIndex = Math.floor(Math.random() * currentIndex);
-    const obj = arr.splice(ramdomIndex, 1);
+    obj = arr.splice(ramdomIndex, 1);
     this.questions.push(obj[0]);
   }
 };
--- src/content/questions.js
+++ src/content/questions.js
@@ -1,51 +1,63 @@
 const QEASY = [
   {
     code: "<code class= 'javascript'> Question 1 <code>",
-    result: 'true'
+    result: 'true',
+    points: 100
   },
   {
     code: "<code class= 'javascript'> Question 2 <code>",
-    result: 'true'
+    result: 'true',
+    points: 100
   },
   {
     code: "<code class= 'javascript'> Question 3 <code>",
-    result: 'true'
+    result: 'true',
+    points: 100
   },
   {
     code: "<code class= 'javascript'> Question 4 <code>",
-    result: 'true'
+    result: 'true',
+    points: 100
   },
   {
     code: "<code class= 'javascript'> Question 5 <code>",
-    result: 'true'
+    result: 'true',
+    points: 100
   },
   {
     code: "<code class= 'javascript'> Question 6 <code>",
-    result: 'true'
+    result: 'true',
+    points: 100
   },
   {
     code: "<code class= 'javascript'> Question 7 <code>",
-    result: 'true'
+    result: 'true',
+    points: 100
   },
   {
     code: "<code class= 'javascript'> Question 8 <code>",
-    result: 'true'
+    result: 'true',
+    points: 100
   },
   {
     code: "<code class= 'javascript'> Question 9 <code>",
-    result: 'true'
+    result: 'true',
+    points: 100
   },
   {
     code: "<code class= 'javascript'> Question 10 <code>",
-    result: 'true'
+    result: 'true',
+    points: 100
   },
   {
     code: "<code class= 'javascript'> Question 11 <code>",
-    result: 'true'
+    result: 'true',
+    points: 100
   },
   {
     code: "<code class= 'javascript'> Question 12 <code>",
-    result: 'true'
+    result: 'true',
+    points: 100
   }
 ];
 let player1Questions = [];
@@ -53,33 +65,23 @@ let player2Questions = [];
 
 
 function sliceQuestions(arr) {
-  let middle = arr.length / 2;
-  let end = arr.length;
-  player1Questions = arr.slice(0, middle);
-  player2Questions = arr.slice(middle, end);
+  const MIDDLE = arr.length / 2;
+  const END = arr.length;
+  player1Questions = arr.slice(0, MIDDLE);
+  player2Questions = arr.slice(MIDDLE, END);
 }
 
 function shuffleQuestions(arr) {
-  const arrQuestions = arr;
+  const ARRQ = arr;
   let currentIndex = arr.length;
   let temporaryValue;
   let ramdomIndex;
   while (currentIndex !== 0) {
     ramdomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
-    temporaryValue = arrQuestions[currentIndex];
-    arrQuestions[currentIndex] = arrQuestions[ramdomIndex];
-    arrQuestions[ramdomIndex] = temporaryValue;
+    temporaryValue = ARRQ[currentIndex];
+    ARRQ[currentIndex] = ARRQ[ramdomIndex];
+    ARRQ[ramdomIndex] = temporaryValue;
   }
-  sliceQuestions(arrQuestions);
+  sliceQuestions(ARRQ);
 }
-
-
-
-// code: "<code class= 'javascript'>  function ClockBomb() {<br>"
-//     + '    this.seconds = 600 <br>'
-//     + '    this.minClock = 10;<br>'
-//     + '    this.secClock = 0;<br>'
-//     + '    this.error = 0;'
-//     + '</code>',
-//   result: 'true'
